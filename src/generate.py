727b151867281cd68ede1b6d2823131db4d521f7
krisciu <krisciu@gmail.com>
2022-10-23 23:40:55 -0500

    adjust model

--- src/generate.py
+++ src/generate.py
@@ -21,13 +21,12 @@ sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               dataset=file_name,
               model_name='355M',
-              steps=2000,
+              steps=500,
               restore_from='fresh',
               run_name='run1',
               print_every=10,
               sample_every=500,
               save_every=500,
-              use_memory_saving_gradients = True,
 	          only_train_transformer_layers = True,
 	          accumulate_gradients = 1
               )
