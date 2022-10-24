745ab7eaa9c982f5227ee06fa704b5cba98939ec
krisciu <krisciu@gmail.com>
2022-10-24 03:33:17 -0500

    fin test

--- src/generate.py
+++ src/generate.py
@@ -62,8 +62,7 @@ gpt2.generate_to_file(sess,
 
 
 f = open("gentext.txt", "r")
-print(f.read())
 
-response = client.create_tweet(text=str(f.read()))
+response = client.create_tweet(text=f.read())
 
 print('response = ' + response)
