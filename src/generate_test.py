725fbe86d6808a11cf6c0b16b4ee3c878a66471d
krisciu <krisciu@gmail.com>
2022-10-24 03:11:34 -0500

    test

---
+++ src/generate_test.py
@@ -0,0 +1,22 @@
+
+import tweepy as tweepy
+import os
+
+
+# Authenticate to Twitter
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+bearer_token = os.getenv('BEARER_TOKEN')
+
+
+client = tweepy.Client(bearer_token,consumer_key,consumer_secret,access_token,access_token_secret)
+
+
+f = open("testtext.txt", "r")
+print(f.read())
+
+response = client.create_tweet(text=str(f.read()))
+
+print('response = ' + response)
